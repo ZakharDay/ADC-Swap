68ab522cad38e8743e658d9f1de82d5e30bbc851
ALeo75C <za.iz.pr@yandex.kz>
2021-01-31 21:45:05 +0300

    WIP

--- app/controllers/api/v1/minors_controller.rb
+++ app/controllers/api/v1/minors_controller.rb
@@ -1,22 +1,38 @@
 class Api::V1::MinorsController < ApplicationController
   def index
-    minors = Minor.all
+    cities = City.all
 
-    minors_data = []
+    page_data = []
 
-    # minors.each do |minor|
-    #   data = minor.card_index
-    #   # data[:url] = api_v1_exchange_minor_url(exchange_minor)
-    #   exchange_minors_data << data
-    # end
+    cities.each do |city|
+      city_name = city.name
+      minors = city.minors
 
-    render json: minors
+      minors_data = []
+      minors.each do |minor|
+        minors_data.push({name: minor.name, id: minor.id, year: minor.start_year, address: minor.address, credits:minor.credits, url: api_v1_minor_url(minor.id)})
+      end
+
+      data = {city: city_name, minors:minors_data}
+      # data[:url] = api_v1_exchange_minor_url(exchange_minor)
+      page_data << data
+    end
+
+
+    render json: page_data
   end
 
-  # def show
-  #   exchange_minor = ExchangeMinor.find(params[:id])
-  #   exchange_minor_data = exchange_minor.card_show
-  #
-  #   render json: exchange_minor_data
-  # end
+  def show
+    minor = Minor.find(params[:id])
+
+    minor_data = {name: minor.name,
+    credits: minor.credits,
+    adress: minor.address,
+    responsible: minor.responsible,
+    responsibleForTheMinor: minor.name,
+    minorDescription: minor.description,
+    minorUrl: minor.url}
+
+    render json: minor_data
+  end
 end
